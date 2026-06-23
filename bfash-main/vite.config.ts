5fa30ebf78d40c04e814ff7195261b7de49f9c78
bilalmukhtar006 <bilalmukhtar006@gmail.com>
2026-06-22 17:29:30 -0700

    vite update

--- bfash-main/vite.config.ts
+++ bfash-main/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import { copyFileSync } from "fs";
 import { resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
