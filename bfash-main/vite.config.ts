55b9bc6418f798cd29ac0570317bbdf351e11335
bilalmukhtar006 <bilalmukhtar006@gmail.com>
2026-06-24 10:33:36 -0700

    vite update

--- bfash-main/vite.config.ts
+++ bfash-main/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, resolve } from "path";
 import { copyFileSync } from "fs";
-import { resolve } from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -16,10 +15,10 @@ export default defineConfig({
         try {
           copyFileSync(
             resolve(__dirname, "public/_redirects"),
-            resolve(__dirname, "dist/_redirects"),
+            resolve(__dirname, "dist/_redirects")
           );
           console.log("✅ _redirects copied to dist!");
-        } catch (e) {
+        } catch (_e) {
           console.log("⚠️ _redirects copy skipped");
         }
       },
