a6b3fc545f93b03b4dd53ddadad50fec8cf5621a
BFash.US <bilalmukhtar006@gmail.com>
2026-08-10 16:39:18 -0700

    vite 2

--- bfash-main/vite.config.ts
+++ bfash-main/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { routerPlugin } from '@tanstack/router-plugin/vite';
+import TanStackRouterPlugin from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    routerPlugin(),
+    TanStackRouterPlugin(),
     tailwindcss(),
     react(),
     tsconfigPaths(),
