a5c013a394f735dbf4859a5fec7219dc78dbfeff
BFash.US <bilalmukhtar006@gmail.com>
2026-09-04 19:19:46 +0500

    vite

--- bfash-main/vite.config.ts
+++ bfash-main/vite.config.ts
@@ -20,4 +20,10 @@ export default defineConfig({
     // Output to bfash-main/dist
     outDir: "../dist",
   },
+
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; frame-src 'self' https://www.google.com; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://analytics.ahrefs.com; style-src 'self' 'unsafe-inline' https://bfash.us https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https:;"
+    }
+  }
 });
