74a7f05ebf0ff22d434fafaaf755f95a11edcf18
BFash.US <bilalmukhtar006@gmail.com>
2026-09-11 16:14:28 +0000

    vite up

--- bfash-main/vite.config.ts
+++ bfash-main/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackRouter } from "@tanstack/router-plugin/vite"; // <-- ADD THIS
 
 export default defineConfig({
   // index.html is inside src
@@ -11,6 +12,7 @@ export default defineConfig({
   publicDir: "../public",
 
   plugins: [
+    tanstackRouter(), // <-- ADD THIS (Must be before react())
     tailwindcss(),
     react(),
     tsconfigPaths(),
